2d97034500dee7160dd3331a24406ea62a41aa8b
Mohmansy94 <77630176+Mohmansy94@users.noreply.github.com>
2021-12-05 11:58:53 +0200

    Add files via upload

---
+++ dms_to_degree.m
@@ -0,0 +1,12 @@
+% Ex4 
+
+function [decimal_deg] = dms_to_degree (deg , minute , second)
+  
+  %% convert from vectors of degree, minute, decimal second to a vector of angles with unit decimal degree.
+  
+  deg = abs (deg);
+  minute = abs(minute);
+  second = abs(second);
+  decimal_deg = deg + minute./60 + second./360;
+  
+endfunction
